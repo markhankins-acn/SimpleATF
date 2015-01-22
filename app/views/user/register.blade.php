ef52f072b1636e60ffb5d0d5560c1c6bd590c38b
Taskforcedev <taskforce2eu@gmail.com>
2015-01-22 15:14:17 +0000

    Create registration page.

---
+++ app/views/user/register.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/master')
+@section('content')
+    <h1>Register</h1>
+
+    <input id="name" type="text" placeholder="Username" />
+    <input id="email" type="email" placeholder="E-Mail Address" />
+    <input id="password" type="password" placeholder="Password" />
+    <input id="password_confirmation" type="password" placeholder="Confirm Password" />
+@stop
