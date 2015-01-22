5f1e4af991c3ed4062f1f7f54e0a5daae846cb72
Taskforcedev <taskforce2eu@gmail.com>
2015-01-22 16:12:37 +0000

    Improve registration page.

--- app/views/user/register.blade.php
+++ app/views/user/register.blade.php
@@ -2,8 +2,46 @@
 @section('content')
     <h1>Register</h1>
 
-    <input id="name" type="text" placeholder="Username" />
-    <input id="email" type="email" placeholder="E-Mail Address" />
-    <input id="password" type="password" placeholder="Password" />
-    <input id="password_confirmation" type="password" placeholder="Confirm Password" />
+
+    <div class="form-group">
+        <input id="name" class="form-control" type="text" placeholder="Username" />
+    </div>
+
+    <div class="form-group">
+        <input id="email" class="form-control" type="email" placeholder="E-Mail Address" />
+    </div>
+
+    <div class="form-group">
+        <input id="password" class="form-control" type="password" placeholder="Password" />
+    </div>
+
+    <div class="form-group">
+        <input id="password_confirmation" class="form-control" type="password" placeholder="Confirm Password" />
+    </div>
+
+    <div class="form-group">
+        <button class="btn-primary btn" onclick="createUser()">Register</button>
+    </div>
+    <script>
+        function createUser()
+        {
+            var data = {};
+            $.each([ "name", "password", "email", "password_confirmation" ], function(field) {
+                data.field = $('#' + field).val();
+            });
+
+            <?php
+                $post_data = [
+                    'to' => 'users',
+                    'data' => 'data',
+                    'success' => 'function() {
+                        window.location = "/";
+                    }
+                    '
+                ];
+            ?>
+
+            @include('jQuery/_post', $post_data)
+        }
+    </script>
 @stop
