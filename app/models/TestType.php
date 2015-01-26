4e73f46bce5501a760b10fc88b7b8e589eee9443
Taskforcedev <taskforce2eu@gmail.com>
2015-01-26 14:54:52 +0000

    Add Test types.

---
+++ app/models/TestType.php
@@ -0,0 +1,13 @@
+<?php
+
+class TestType
+{
+    public static function getTestTypes()
+    {
+        $data = [
+            ['id' => 1, 'name' => 'hasText'],
+            ['id' => 2, 'name' => 'idHasText'],
+        ];
+        return $data;
+    }
+}
