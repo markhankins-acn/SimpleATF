be942065ce5d1fc73ff2e140af0dddcbe92d9a7f
Taskforcedev <taskforce2eu@gmail.com>
2015-01-26 13:02:47 +0000

    Moved get id route before get.

--- app/routes.php
+++ app/routes.php
@@ -3,5 +3,5 @@ Route::get('/', 'HomeController@index');
 
 /* Api */
 Route::post('post', 'ApiController@postItem');
-Route::get('get', 'ApiController@getItems');
 Route::get('get/{id}', 'ApiController@getItem');
+Route::get('get', 'ApiController@getItems');
