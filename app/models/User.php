63bbd2cea9b3715aa5a67fb4812c2b4f21e6a9b5
Taskforcedev <taskforce2eu@gmail.com>
2015-01-26 10:37:52 +0000

    Add user validation and safe creation.

--- app/models/User.php
+++ app/models/User.php
@@ -5,22 +5,69 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface
+{
+    use UserTrait, RemindableTrait;
 
-	use UserTrait, RemindableTrait;
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    protected $fillable = ['name', 'password', 'email'];
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
 
+    /**
+     * @var array $rules Rules for validation.
+     */
+    private $rules = [
+        'name' => ['required', 'unique:users,username'],
+        'email' => ['required', 'unique:users,email'],
+        'password' => ['required', 'min:6']
+    ];
+
+    /**
+     * Determine if the data provided is valid.
+     *
+     * @param $data
+     *
+     * @return mixed
+     */
+    public function isValid($data)
+    {
+        return Validator::make($data, $this->rules);
+    }
+
+    /**
+     * \Log::error("User data failed validation.");
+     *
+     * @param $data
+     *
+     * @return bool
+     */
+    public static function safeCreate($data)
+    {
+        $user = new User();
+        if ($user->isValid($data)) {
+            $data['password'] = Hash::make($data['password']);
+            try {
+                $user = self::create($data);
+                return true;
+            } catch (Exception $e) {
+                \Log::error("Unable to create user.");
+                return false;
+            }
+        } else {
+            \Log::error("User data failed validation.");
+            return false;
+        }
+    }
 }
