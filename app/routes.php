76342beafdc4ea89ed6d21b7f9badfee23e0fa35
Taskforcedev <taskforce2eu@gmail.com>
2015-01-26 14:54:13 +0000

    Add routes.

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,13 @@
 <?php
-Route::get('/', 'HomeController@index');
+Route::get('/', ['as' => 'projects.index', 'uses' => 'HomeController@index']);
 
 /* Api */
 Route::post('post', 'ApiController@postItem');
 Route::get('get/{id}', 'ApiController@getItem');
 Route::get('get', 'ApiController@getItems');
+
+/* Projects */
+Route::get('/project/{id}', ['as' => 'project.view', 'uses' => 'ProjectController@show']);
+
+/* Test Suites */
+Route::get('/test_suite/{id}', ['as' => 'suite.view', 'uses' => 'TestSuiteController@show']);
