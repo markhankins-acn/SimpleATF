a15aea21504bc05c095c31123ea970dd2581de48
Taskforcedev <taskforce2eu@gmail.com>
2015-01-29 09:46:21 +0000

    add hasJsonKey and hasJson

--- app/models/TestType.php
+++ app/models/TestType.php
@@ -5,7 +5,9 @@ class TestType
     public $types = [
         1 => ['id' => 1, 'name' => 'hasText'],
         2 => ['id' => 2, 'name' => 'idHasText'],
-        3 => ['id' => 2, 'name' => 'hasStatusCode'],
+        3 => ['id' => 3, 'name' => 'hasStatusCode'],
+        4 => ['id' => 4, 'name' => 'hasJsonKey'],
+        5 => ['id' => 5, 'name' => 'hasJson'],
     ];
 
     public function getTestTypes()
